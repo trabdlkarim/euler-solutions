6108b5a0707dc3b25236019410251c3fdfd73734
TOURE A. KARIM <dev@trabdlkarim.me>
2025-08-16 14:40:54 +0200

    Add solution for problem #1

---
+++ solutions/P0001.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def compute():
+    sum = 0
+    for i in range(1,1000):
+        if i% 3 == 0 or i%5 == 0:
+            sum = sum + i
+    return sum
+
+
+def main():
+    print(compute())
+
+if __name__ =="__main__":
+    main()
